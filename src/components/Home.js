38ef75e586186f1610fae22363c0c8c9b83c8219
Vishnupriya <vishnupriya.t7@gmail.com>
2023-03-29 15:48:18 +0530

    card-completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Card, Button } from "react-bootstrap";
+import { Row, Col, Card, Button, Container } from "react-bootstrap";
 
 function Home() {
   const [restaurants, setRestaurants] = useState([]);
@@ -7,7 +7,7 @@ function Home() {
   useEffect(() => {
     let restaurants = async () => {
       let resData = await fetch("./restaurants.json");
-           let res = await resData.json();
+      let res = await resData.json();
       setRestaurants(res.restaurants);
       // console.log(res.restaurants);
     };
@@ -15,35 +15,32 @@ function Home() {
   }, []);
 
   return (
-    <Row xs={1} md={4} className="g-4 row-cols-1">
-      {restaurants &&
-        restaurants.map(restaurant =>
-          <Col
-            className="d-flex align-items-center justify-content-center h-25"
-           
-            key={restaurant.id}
-          >
-            <Card style={{ width: "20rem" }}>
-              <Card.Header>
-                {restaurant.cuisine_type}
-              </Card.Header>
-              <Card.Img variant="top" style={{height: "20rem"}} src={restaurant.photograph} />
-              <Card.Body>
-                <Card.Title>
-                  {restaurant.name}
-                </Card.Title>
-                <Card.Text>
-                  {restaurant.address}
-                </Card.Text>
-                <Card.Text>
-                  {restaurant.neighborhood}
-                </Card.Text>
-                <Button variant="primary">Explore</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        )}
-    </Row>
+    <Container>
+      <Row xs={1} md={2} lg={3} xl={4} className="g-4 row-cols-1">
+        {restaurants &&
+          restaurants.map((restaurant) => (
+            <Col
+              className="d-flex align-items-center justify-content-center h-25"
+              key={restaurant.id}
+            >
+              <Card style={{ width: "20rem" }}>
+                <Card.Header>{restaurant.cuisine_type}</Card.Header>
+                <Card.Img
+                  variant="top"
+                  style={{ height: "20rem" }}
+                  src={restaurant.photograph}
+                />
+                <Card.Body>
+                  <Card.Title>{restaurant.name}</Card.Title>
+                  <Card.Text>{restaurant.address}</Card.Text>
+                  <Card.Text>{restaurant.neighborhood}</Card.Text>
+                  <Button variant="primary">Explore</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+      </Row>
+    </Container>
   );
 }
 
